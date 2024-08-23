fb4dc12d13a9bea9a908039cef805a7855177106
Sriram-Villuri <sriramnandanvilluri@gmail.com>
2024-08-23 18:42:35 +0530

    read now

---
+++ practice1 variables.py
@@ -0,0 +1,61 @@
+score = 110
+
+snails = 3
+
+(score > 100) and (snails >= 4)
+
+print (score > 100) and (snails >= 4)
+
+x = 5
+
+y = x * 3
+
+print (y)
+
+greeting = "Hello "
+
+name = "Martin"
+
+message = greeting + name
+
+print(message)
+
+print (len(message))
+
+card1 = '1 heart'
+
+card2 = '2 hearts'
+
+card3 = '3 hearts'
+
+card4 = '4 hearts'
+
+card5 = "5 hearts"
+
+cards = ["1 heart" , "4 hearts" , "2 hearts", "3 hearts" , "5 hearts"]
+
+print (cards[0])
+
+print (cards[4])
+
+spells = 4444444444444444444447
+
+if (spells > 10):
+    print ("You gained the fire enchantment")
+
+else :
+    print ("You ganined nothing")
+
+ghosts =  100
+if (ghosts > 1):
+    print ("its so spoooooky!")
+
+elif (ghosts < 0):
+    print("Get that ghost!!")
+
+else :
+    print ("ghosts all gone")
+
+#thank you for reading my practice.py file , now you can move on to the next file named as 'practice2 loops.py'.Please follow my account.
+    
+
---
+++ practice2 loops.py
@@ -0,0 +1,9 @@
+for count in range(1, 11):
+    print ("You are the high scorer")
+
+robots = ["BING", "BLEEP", "BLOOP"]
+
+for robot in robots :
+    print ("I AM A ROBOT. MY NAME IS " + robot)
+
+

5a0d20ee1a1644166fd46d193f83e2633123b362
Sriram-Villuri <sriramnandanvilluri@gmail.com>
2024-08-23 23:07:35 +0530

    hi

---
+++ simple-project1.py
@@ -0,0 +1,25 @@
+
+#This is a simple project where the computer will talk to you about simple questions.
+
+yourname = input("What is your name?")
+
+print ("Hello " + yourname + ", My name is Sriram Nandan the computer")
+
+yourage = input("How old are you " + yourname + "?")
+
+print ("Oh! So you are " + yourage + " years old. I am 10 years old, I was created in 2014")
+
+youroffice = input("Where do you work or learn at " + yourname + "?")
+
+print ("So you work or learn at " + youroffice + "!. I dont work but I go to school. Very funny for a computer to go to school right?")
+
+yourgame = input("What is your favourite video game " + yourname + "?")
+
+print ("Your favourite game is " + yourgame + ". My favourite game is Roblox")
+
+print ("Thank you for talking to me " + yourname + "!. Hope we talk some other day, BYEE!")
+
+#Thank you for using my simple-project.py. If you liked it please follow my account.
+
+
+
---
+++ simple-project2.py
@@ -0,0 +1,16 @@
+
+#This is a simple project where you can insert a number and the computer will tell you all the even numbers till the number you have input.
+
+number = input("Please enter your number : ")
+
+print ("This is the number you have input : " + number)
+
+index = 0
+
+for each in range (0, int (number)+1) :  
+    if index % 2 ==  0 :
+        print (index)
+    index = index + 1
+
+#Thank you for using my simple and very short project. If you liked it please make sure to follow my account.
+    

4b77c0ecf447cd5f4f5ea9634f74b69369b40ae0
Sriram-Villuri <sriramnandanvilluri@gmail.com>
2024-08-23 19:53:58 +0530

    loops and variablses

---
+++ simple_game1.py
@@ -0,0 +1,17 @@
+
+#this is a simple game where you can play if you want a water baloon or not
+
+print ("NARRATOR : The story starts at you in Minecraft....")
+
+answer = input("RUDEGUY213 : Want a water balloon? y/n")
+
+while answer == "y" :
+    print ("value of awnser :",answer)
+    answer =input("RUDEGUY213 : Are you going to throw it y/n")
+    print ("NARRATOR : You throw the water balloon at GREENGUY156")
+
+# a = 1
+
+# while a <= 5 :
+#     print ("a is ",a)
+#     a = a

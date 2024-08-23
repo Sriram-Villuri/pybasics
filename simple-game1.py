ba07382d2b99d8feca82c9d4456d3216ffa2b1e2
Sriram-Villuri <sriramnandanvilluri@gmail.com>
2024-08-23 21:40:09 +0530

    reead now

---
+++ simple-game1.py
@@ -0,0 +1,22 @@
+
+#this is a simple game where you can play if you want a water baloon or not
+
+print ("NARRATOR : The story starts at you in Minecraft....")
+
+answer = input("RUDEGUY213 : Want a water balloon? y/n")
+
+if answer == "y" :
+     #print ("value of awnser :",answer)
+     answer =input("RUDEGUY213 : Are you going to throw it y/n")
+else :
+     print ("RUDEGUY213 : Urghhh!")
+     print ("NARRATOR : RUDEGUY213 makes an annoyed face and throws the water baloon at you, now your all wet!")
+     print ("GAME : YOU GOT WET")
+     print ("       GAME OVER")
+
+if answer == "y" :
+     print ("NARRATOR : You throw the water balloon at GREENGUY156")
+     print ("GREENGUY156 : Hey! Why did you throw it at me! I hate you!")
+
+#Thank you for playing my simple-game.py. Now you can move on to another simple-game.py or continue learning. If you liked my game please follow my account.
+
